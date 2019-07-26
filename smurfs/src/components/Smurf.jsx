fa36b854487912e427e36197b99e8ef5833cf41a
Michael Harris <mhguitar1997@gmail.com>
2019-07-26 10:08:24 -0700

    Edit button invokes dispatch

--- smurfs/src/components/Smurf.jsx
+++ smurfs/src/components/Smurf.jsx
@@ -1,8 +1,12 @@
 import React, { useContext } from "react";
-import {SmurfContext} from '../contexts/SmurfContext'
+import { SmurfContext } from "../contexts/SmurfContext";
 
 const Smurf = ({ smurf }) => {
-    const {disptach} = 
+  const { disptach } = useContext(SmurfContext);
+
+  const edit = () => {
+      disptach({type: "EDIT_ON", payload: smurf.id})
+  }
   return (
     <div>
       <h2>{smurf.name}</h2>
@@ -10,6 +14,7 @@ const Smurf = ({ smurf }) => {
         <h4>Age: {smurf.age}</h4>
         <h4>Height: {smurf.height}cm</h4>
       </aside>
+      <button onClick={edit}>Edit</button>
     </div>
   );
 };
