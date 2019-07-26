7ef63ce31e9bcb9bce87cf58319f2664c85c4b01
Michael Harris <mhguitar1997@gmail.com>
2019-07-26 09:13:30 -0700

    Form set up

--- smurfs/src/components/SmurfForm.jsx
+++ smurfs/src/components/SmurfForm.jsx
@@ -1,20 +1,48 @@
 import React, { useState } from "react";
 
 const SmurfForm = () => {
+  const [formState, setFormState] = useState({
+    name: "",
+    age: "",
+    height: ""
+  });
+
+  const handleChange = input => {
+      setFormState({
+          ...formState,
+          [input.target.name]: input.target.value
+      })
+  };
   return (
     <form>
       <fieldset>
         <legend>Add your smurf</legend>
         <label htmlFor="">Name</label>
-        <input type="text" placeholder="Name" name="name" autoComplete="off" />
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          autoComplete="off"
+          value={formState.name}
+          onChange={handleChange}
+        />
         <label htmlFor="">Age</label>
-        <input type="number" placeholder="Age" name="age" autoComplete="off" />
+        <input
+          type="number"
+          placeholder="Age"
+          name="age"
+          autoComplete="off"
+          value={formState.age}
+          onChange={handleChange}
+        />
         <label htmlFor="">Height</label>
         <input
           type="number"
           placeholder="Height"
           name="height"
           autoComplete="off"
+          value={formState.height}
+          onChange={handleChange}
         />
       </fieldset>
     </form>
