1770a7bce81f9d45662081447721164275fed28b
Michael Harris <mhguitar1997@gmail.com>
2019-07-26 08:32:28 -0700

    Smurf Provider import an will wrap all other smuirf components

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -3,12 +3,17 @@ import { SmurfProvider } from "../contexts/SmurfContext";
 import "./App.css";
 
 const App = () => {
+
+  useEffect(() => {
+
+  }, [])
   return (
     <div className="App">
       <h1>SMURFS! 2.0 W/ Redux</h1>
-      <div>Welcome to your state management version of Smurfs!</div>
-      <div>Start inside of your `src/index.js` file!</div>
-      <div>Have fun!</div>
+
+      <SmurfProvider value={}>
+
+      </SmurfProvider>
     </div>
   );
 };
