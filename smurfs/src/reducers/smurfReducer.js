ea34ae16ce36f75a076efa32bd9ba140922017ec
Michael Harris <mhguitar1997@gmail.com>
2019-07-26 09:35:20 -0700

    Case add for add error

--- smurfs/src/reducers/smurfReducer.js
+++ smurfs/src/reducers/smurfReducer.js
@@ -30,13 +30,19 @@ export const smurfReducer = (state, { type, payload }) => {
         ...state,
         loading: true
       };
-      case "ADD_SUCCESS":
-          return {
-              ...state,
-              loading: false,
-              smurfs: payload,
-              message: "Your smurf was added"
-          }
+    case "ADD_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        smurfs: payload,
+        message: "Your smurf was added"
+      };
+    case "ADD_ERROR":
+      return {
+        ...state,
+        loading: false,
+        error: payload
+      };
     default:
       return state;
   }
