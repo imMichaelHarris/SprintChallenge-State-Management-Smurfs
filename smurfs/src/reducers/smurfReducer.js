f255bef2b35b87ac633cc2c919b9580d0e77e22f
Michael Harris <mhguitar1997@gmail.com>
2019-07-26 08:56:16 -0700

    Fetch function will dispatch based on promise resolution

--- smurfs/src/reducers/smurfReducer.js
+++ smurfs/src/reducers/smurfReducer.js
@@ -1,8 +1,16 @@
 export const initialSmurfState = {
-  smurfs: []
+  smurfs: [],
+  loading: false,
+  error: ""
 };
 export const smurfReducer = (state, { type, payload }) => {
   switch (type) {
+      case "FETCH_START":
+          return {
+              ...state,
+              loading: true,
+              error: ""
+          }
     default:
       return state;
   }
