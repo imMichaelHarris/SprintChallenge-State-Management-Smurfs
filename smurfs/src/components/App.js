aa9f72aacb229cb3df732b10a92fb5e9da631490
Michael Harris <mhguitar1997@gmail.com>
2019-07-26 08:59:32 -0700

    Render smurf list in App.js

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,34 +1,34 @@
 import React, { useEffect, useReducer } from "react";
 import { SmurfProvider } from "../contexts/SmurfContext";
-import { initialSmurfState, smurfReducer } from "../reducers/smurfReducer";
-import { myAxios } from "../myAxios";
+import {initialSmurfState, smurfReducer} from '../reducers/smurfReducer'
+import {myAxios} from '../myAxios';
+import SmurfList from "../views/SmurfList";
 import "./App.css";
 
 const App = () => {
   const [state, dispatch] = useReducer(smurfReducer, initialSmurfState);
 
   const getSmurfs = () => {
-    dispatch({ type: "FETCH_START" });
-    myAxios
-      .get("/smurfs")
-      .then(res => {
-        dispatch({ type: "FETCH_SUCCESS", payload: res.data });
-        console.log(res);
-      })
-      .catch(err => {
-        console.log(err.response);
-        dispatch({ type: "FETCH_ERROR", payload: err.response.data });
-      });
-  };
+    dispatch({type: "FETCH_START"})
+    myAxios.get('/smurfs').then(res => {
+      dispatch({type: "FETCH_SUCCESS", payload: res.data})
+      console.log(res)
+    }).catch(err => {
+      console.log(err.response)
+      dispatch({type: "FETCH_ERROR", payload: err.response.data})
+    })
+  }
 
   useEffect(() => {
-    getSmurfs();
-  }, []);
+    getSmurfs()
+  }, [])
   return (
     <div className="App">
       <h1>SMURFS! 2.0 W/ Redux</h1>
 
-      <SmurfProvider value={state.smurfs} />
+      <SmurfProvider value={state.smurfs}>
+        <SmurfList />
+      </SmurfProvider>
     </div>
   );
 };
